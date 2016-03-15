7eae7ebfa9a660bd985e1a3f7b7aba096b48de18
Arvind Padmanabhan <arvindpdmn@gmail.com>
2016-03-15 19:36:13 +0530

    Lua multiline comments syntax updated since [[]] comments are not found to execute. GPIO comment corrected.

--- NodeMCU/Blink/Blink.lua
+++ NodeMCU/Blink/Blink.lua
@@ -1,28 +1,28 @@
---[[=====================================================================
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  ---------------------------------------------------------------------
-  Purpose : Blink an LED via a Lua script. Wi-Fi connection is enabled
-            but is not used in this example.
-  Date    : 25 Jan 2016
-  Boards  : NodeMCU
-  =====================================================================
---]]
+--=====================================================================
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU General Public License
+--along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-----------------------------------------------------------------------
+--Purpose : Blink an LED via a Lua script. Wi-Fi connection is enabled
+--          but is not used in this example.
+--Date    : 25 Jan 2016
+--Boards  : NodeMCU
+--=====================================================================
+--
 
 -----------------------------------------------------------------------
 -- Globals
 -----------------------------------------------------------------------
-local pin = 2            --> GPIO2
+local pin = 2            --> GPIO4
 local value = gpio.LOW
 local duration = 1000    --> 1 second
 
